6d6c2dc574b27b8a5a6a15582207274900d648e2
GitHub Action <action@github.com>
2026-08-26 17:43:46 +0000

    Initialize template repository proper
    Set package name to com.github.davishi25.statcheck
    Set project name to StatCheck
    Set modid to statcheck

--- src/main/java/com/github/davishi25/statcheck/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/davishi25/statcheck/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.davishi25.statcheck.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
