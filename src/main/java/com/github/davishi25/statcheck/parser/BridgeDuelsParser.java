3f7760ce03e88df020e617b5fbcda4bb39c6783f
davishi25 <126983431+davishi25@users.noreply.github.com>
2026-08-28 13:37:08 -0500

    feat: cleaned up bridge duels parser

--- src/main/java/com/github/davishi25/statcheck/parser/BridgeDuelsParser.java
+++ src/main/java/com/github/davishi25/statcheck/parser/BridgeDuelsParser.java
@@ -7,29 +7,28 @@ public class BridgeDuelsParser implements ApiParser{
     public static class BridgeDuelsContext {
         private final JsonObject bridgeDuelsObj;
         BridgeDuelsContext(JsonObject bridgeDuelsObj) { this.bridgeDuelsObj = bridgeDuelsObj; }
+
         public int getWins() {
-            return getSoloWins() + getDoublesWins() + getThreesWins() + getFoursWins() + getFourTeamsWins() + getCTFWins();
+            return getWins("duel") + getWins("doubles") + getWins("threes") +
+                   getWins("four") + getWins("2v2v2v2") + getWins("3v3v3v3") + getCTFWins();
         }
+        public int getWins(String mode) { return Util.safeGetInt(bridgeDuelsObj,"bridge_" + mode + "_wins"); }
+
         public int getLosses() {
-            return getSoloLosses() + getDoublesLosses() + getThreesLosses() + getFoursLosses() + getFourTeamsLosses() + getCTFLosses();
+            return getLosses("duel") + getLosses("doubles") + getLosses("threes") +
+                    getLosses("four") + getLosses("2v2v2v2") + getLosses("3v3v3v3") + getCTFLosses();
         }
+        public int getLosses(String mode) { return Util.safeGetInt(bridgeDuelsObj,"bridge_" + mode + "_losses"); }
+
+        public int getCTFWins() { return Util.safeGetInt(bridgeDuelsObj,"capture_threes_wins"); }
+        public int getCTFLosses() { return Util.safeGetInt(bridgeDuelsObj,"capture_threes_losses"); }
+
         public double getWLR() {
             int losses = getLosses();
             return (double)getWins() / (losses != 0 ? losses : 1);
         }
 
-        public int getSoloWins() { return Util.safeGetInt(bridgeDuelsObj,"bridge_duel_wins"); }
-        public int getDoublesWins() { return Util.safeGetInt(bridgeDuelsObj,"bridge_doubles_wins"); }
-        public int getThreesWins() { return Util.safeGetInt(bridgeDuelsObj,"bridge_threes_wins"); }
-        public int getFoursWins() { return Util.safeGetInt(bridgeDuelsObj,"bridge_four_wins"); }
-        public int getFourTeamsWins() { return Util.safeGetInt(bridgeDuelsObj,"bridge_3v3v3v3_wins") + Util.safeGetInt(bridgeDuelsObj,"bridge_2v2v2v2_wins"); }
-        public int getCTFWins() { return Util.safeGetInt(bridgeDuelsObj,"capture_threes_wins"); }
-        public int getSoloLosses() { return Util.safeGetInt(bridgeDuelsObj,"bridge_duel_losses"); }
-        public int getDoublesLosses() { return Util.safeGetInt(bridgeDuelsObj,"bridge_doubles_losses"); }
-        public int getThreesLosses() { return Util.safeGetInt(bridgeDuelsObj,"bridge_threes_losses"); }
-        public int getFoursLosses() { return Util.safeGetInt(bridgeDuelsObj,"bridge_four_losses"); }
-        public int getFourTeamsLosses() { return Util.safeGetInt(bridgeDuelsObj,"bridge_3v3v3v3_losses") + Util.safeGetInt(bridgeDuelsObj,"bridge_2v2v2v2_losses"); }
-        public int getCTFLosses() { return Util.safeGetInt(bridgeDuelsObj,"capture_threes_losses"); }
+
     }
     public String getStatLine(JsonObject playerObj) {
         BridgeDuelsContext ctx = new BridgeDuelsContext(playerObj.getAsJsonObject("Duels"));
